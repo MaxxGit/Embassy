d52176b49653a50d7856f987dd0d08f7cdd375e2
MaxxBit <massimiliano.vergnetta@gmail.com>
2016-11-30 23:20:14 +0100

    Add info map

--- app/src/main/java/stm/com/embassy/InfoActivity.java
+++ app/src/main/java/stm/com/embassy/InfoActivity.java
@@ -1,8 +1,8 @@
 package stm.com.embassy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -21,8 +21,10 @@ public class InfoActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(getApplicationContext(), InfoMapsActivity.class);
+                startActivity(intent);
             }
         });
     }
