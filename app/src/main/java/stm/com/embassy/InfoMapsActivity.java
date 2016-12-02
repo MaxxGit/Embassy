ab4459556df273ab902a0de1c1200ebadb512a16
BridgeGit <fabiobridge@gmail.com>
2016-12-02 16:50:23 +0100

    Disabilitate le mappe per problema con versioni vecchie

--- app/src/main/java/stm/com/embassy/InfoMapsActivity.java
+++ app/src/main/java/stm/com/embassy/InfoMapsActivity.java
@@ -3,26 +3,27 @@ package stm.com.embassy;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.OnMapReadyCallback;
+//import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
 
-public class InfoMapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class InfoMapsActivity extends FragmentActivity //implements OnMapReadyCallback
+{
 
-    private GoogleMap mMap;
+    //private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                //.findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
     }
 
 
@@ -35,18 +36,18 @@ public class InfoMapsActivity extends FragmentActivity implements OnMapReadyCall
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(41.924511, 12.516278);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Ambasciata della Tunisia")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
-        );
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 11.0f));
-
-    }
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+//        mMap = googleMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(41.924511, 12.516278);
+//        mMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Ambasciata della Tunisia")
+//                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+//        );
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 11.0f));
+//
+//    }
 }
