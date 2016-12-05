8e7438041c2577f5b3b66ec305858bae99ba0bc1
MaxxBit <massimiliano.vergnetta@gmail.com>
2016-12-05 11:00:28 +0100

    Aggiunto log alla classe MainActivity

--- app/src/main/java/stm/com/embassy/MainActivity.java
+++ app/src/main/java/stm/com/embassy/MainActivity.java
@@ -3,14 +3,17 @@ package stm.com.embassy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = this.getLocalClassName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i( TAG, "MainActivity onCreate !!!" );
     }
 
     public void onInfoClick(View view){
